bdfffe1522c8a81ffd6e9ea81243bd6392b48592
Macsari11 <maclas871@hengersor.hu>
2025-01-27 13:18:53 +0100

    2025.01.27

--- Backend/gyakorlo/server.js
+++ Backend/gyakorlo/server.js
@@ -1,24 +1,27 @@
-const express=require ("express");
-const app=express();
-const cors = require("cors");
-const mysql=require("mysql");
-const bodyParser=require('body-parser');
+const express = require("express");
+const app = express ();
+const  cors = require("cors");
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
-
+ 
+ 
 app.use(cors());
-
-const db =mysql.createConnection({
-    user:"root",
-    host:"127.0.0.1",
-    port:3306,
-    password:"",
-    database:"kozutak"
+ 
+const db = mysql.createConnection({
+    user: "root",
+    host: "127.0.0.1",
+    port: 3307,
+    password: "",
+    database: "kozutak",
 });
-
-app.get("/",(req,res)=> {
-    res.send("Fut a backend!");
+ 
+ 
+app.get("/", (req, res) =>{
+    res.send("Fut a backend");
 })
-
+ 
+ 
 app.get("/user", (req, res) => {
     const sql = "SELECT * FROM `regiok`";
     db.query(sql, (err, result) =>{
@@ -26,9 +29,10 @@ app.get("/user", (req, res) => {
         return res.json(result)
     })
 })
-
-app.listen(3001, () => (
-    console.log("Server is running on port 3001")
-));
+ 
+app.listen(3001, () => {
+    console.log("Server is running on port 3001");
+});
+ 
  
 
